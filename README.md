74d1d92bd000cd278086e2d9b5335fd7b482ad69
Triston Golden <triston_golden@yahoo.com>
2022-02-09 17:01:29 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Ecommerce-Backend-App
+
+Description
+This app is the backend for an E-Commerce website. The server was controlled by Express,js and the database was controlled by MySQL in conjunction with Sequelize (allowed usage of models and queries). MySQL was used to make tables for categories, products, product tags and tags. RESTful API routes controlled the ability to make changes from the database.
+
+Installation:
+Node.js and MySQL are required for this application to run.
+
+You must be on the root directory and run:
+npm install
+
+To run the server, in the command line run:
+node server.js
+
+Technology Used:
+Node
+MySQL
+Sequelize
+Express
+Dotenv
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,40 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update a tag's name by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(tagData);
+  } catch (error) {}
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(tagData);
+  } catch (error) {}
 });
 
 module.exports = router;
